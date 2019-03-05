a27e126a266de435eca7fd66aff50f6a1b5dcc62
stephenhales <stephen.james.hales@gmail.com>
2019-03-05 13:05:44 -0600

    add conditional logic to jsx.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,18 +7,26 @@ class App extends React.Component {
         super(props);
         
         // THIS IS THE ONLY TIME we do direct assignment to this.state
-        this.state = { lat: null };
-
+        this.state = { lat: null, errorMessage: '' };
+        
         window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat :position.coords.latitude }),
-            (error) => console.log(error)  
+            position => this.setState({ lat :position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })  
         );
     }
-
-
+        
+        
     // React requires a render method
-    render() {
-        return <div>Latitude: {this.state.lat}</div>
+    render() {        
+        if(this.state.errorMessage && !this.state.lat)     {
+            return <div>Error: {this.state.errorMessage}</div>            
+        }
+        
+        if(!this.state.errorMessage && this.state.lat)     {
+            return <div>Latitude: {this.state.lat}</div>            
+        }
+                
+        return <div>Loading</div>                
     }
 }
 
