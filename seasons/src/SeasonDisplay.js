42163db04899f7d88a33d0cf58a4e2c086f2cf50
stephenhales <stephen.james.hales@gmail.com>
2019-03-05 15:27:26 -0600

    add ternary logic to determine the season and icon

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        icon: "sun"
+    },
+    winter: {
+        text: "Burr, it's chilly",
+        icon: "snowflake"
+    }
+
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? "Burr, it's chilly" : "Let's hit the beach";
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
+
     return (
         <div>
-            Season Display
-        </div>
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
+        </div>        
     );
+}
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9 ) {
+        return lat > 0 ? 'summer' : 'winter';
+    }
+    else{
+        return lat > 0 ? 'winter' : 'summer';        
+    }
 }
 
 export default SeasonDisplay;
