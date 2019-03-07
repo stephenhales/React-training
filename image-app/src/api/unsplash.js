9ec401145ec763bd1719971288512275dbd537a0
stephenhales <stephen.james.hales@gmail.com>
2019-03-07 11:34:52 -0600

    rename the project from cars to image app

--- image-app/src/api/unsplash.js
+++ image-app/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID ',        
+    Authorization: 'Client-ID 2641ed2f51a3a72d6f2c6abe10304fd479802c1c3ba526ce01038f53de3300a9',        
   }
 });
